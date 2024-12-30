53cbbaf36c0cd4f517b6d3645c753c937cbec27c
nebarrow <101344206+nebarrow@users.noreply.github.com>
2024-12-30 17:07:22 +0300

    refactor: remove option method

--- src/main/java/com/nebarrow/filter/CorsFilter.java
+++ src/main/java/com/nebarrow/filter/CorsFilter.java
@@ -15,12 +15,9 @@ public class CorsFilter extends HttpFilter {
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
 
         res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH");
         res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
-            res.setStatus(HttpServletResponse.SC_OK);
-            return;
-        }
+
         chain.doFilter(req, res);
     }
 }
