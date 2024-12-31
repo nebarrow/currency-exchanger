9592a8be75e2a19c5d81344b19865c0f039bfe05
nebarrow <101344206+nebarrow@users.noreply.github.com>
2024-12-31 13:20:07 +0300

    refactor: add switch case to clean up code

--- src/main/java/com/nebarrow/filter/CurrenciesFilter.java
+++ src/main/java/com/nebarrow/filter/CurrenciesFilter.java
@@ -1,44 +1,45 @@
 package com.nebarrow.filter;
 
+import com.nebarrow.dto.request.CurrencyRequest;
 import com.nebarrow.util.HttpErrorSender;
+import com.nebarrow.util.Validator;
+import com.nebarrow.validation.ValidationResult;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import com.nebarrow.validation.ParametersValidator;
+import static jakarta.servlet.http.HttpServletResponse.*;
 
 import java.io.IOException;
 
 public class CurrenciesFilter extends HttpFilter {
+    private static final String ALLOWED_METHODS = "GET, POST";
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        if (req.getMethod().equals("POST")) {
-            var name = req.getParameter("name");
-            var code = req.getParameter("code");
-            var sign = req.getParameter("sign");
-
-            var nameMessageError = ParametersValidator.checkName(name);
-            var codeMessageError = ParametersValidator.checkCode(code);
-            var signMessageError = ParametersValidator.checkSign(sign);
-
-            if (!nameMessageError.isEmpty()) {
-                HttpErrorSender.sendError(res, nameMessageError, HttpServletResponse.SC_BAD_REQUEST);
-                return;
-            } else if (!codeMessageError.isEmpty()) {
-                HttpErrorSender.sendError(res, codeMessageError, HttpServletResponse.SC_BAD_REQUEST);
-                return;
-            } else if (!signMessageError.isEmpty()) {
-                HttpErrorSender.sendError(res, signMessageError, HttpServletResponse.SC_BAD_REQUEST);
-                return;
+        switch (req.getMethod()) {
+            case "POST" -> handlePostRequest(req, res, chain);
+            case "GET" -> chain.doFilter(req, res);
+            default -> {
+                res.setHeader("Allow", ALLOWED_METHODS);
+                res.setStatus(SC_METHOD_NOT_ALLOWED);
             }
-            chain.doFilter(req, res);
-            return;
-        } else if (req.getMethod().equals("GET")) {
-            chain.doFilter(req, res);
+        }
+    }
+
+    private void handlePostRequest(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        CurrencyRequest currency = new CurrencyRequest(
+                request.getParameter("code"),
+                request.getParameter("name"),
+                request.getParameter("sign")
+        );
+        ValidationResult validationResult = Validator.validate(currency);
+        if (validationResult.hasErrors()) {
+            HttpErrorSender.sendError(response, validationResult.getMessage(), SC_BAD_REQUEST);
             return;
         }
-        res.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+        request.setAttribute("currencyRequest", currency);
+        chain.doFilter(request, response);
     }
 }
