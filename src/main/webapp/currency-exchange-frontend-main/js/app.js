5a097982dde6738f66f15ea517ab439636eebd67
nebarrow <101344206+nebarrow@users.noreply.github.com>
2024-12-31 13:19:21 +0300

    refactor: change ip to localhost

--- src/main/webapp/currency-exchange-frontend-main/js/app.js
+++ src/main/webapp/currency-exchange-frontend-main/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    const host = "http://77.221.151.66:8080/currency-exchanger"
+    const host = "http://localhost:8080"
 
     // Fetch the list of currencies and populate the select element
     function requestCurrencies() {
